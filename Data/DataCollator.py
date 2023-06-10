9b5202a27c46d9fb4643cba2d9ebc71f8dada3f5
jwengr <jinwoo0766@naver.com>
2023-06-10 11:41:42 +0900

    add

---
+++ Data/DataCollator.py
@@ -0,0 +1,17 @@
+import fugashi
+import mecab_ko_dic
+import numpy as np
+import torch 
+import torch.nn as nn
+
+def DataCollatorForMecabSpanMLM():
+    pass
+
+def DataCollatorForSpanMLM():
+    pass
+
+def DataCollatorForUniformMLM():
+    pass
+
+def DataCollatorWithPadding():
+    pass
