6f26cb762a4ff28ca12eee70d740e4ace363ac81
jwengr <jinwoo0766@naver.com>
2023-06-11 20:52:26 +0900

    collate

--- Data/DataCollator.py
+++ Data/DataCollator.py
@@ -1,17 +1,82 @@
-import fugashi
-import mecab_ko_dic
+import random
 import numpy as np
 import torch 
 import torch.nn as nn
 
-def DataCollatorForMecabSpanMLM():
-    pass
+from tokenizers import Tokenizer
 
-def DataCollatorForSpanMLM():
-    pass
+class DataCollatorForSentencePieceSpanMLM:
+    def __init__(
+            self,
+            tokenizer:Tokenizer,
+            pad_token='[PAD]',
+            mask_prob=0.15,
+            mask_token='[MASK]',
+            metaspace_token='▁',
+            padding_argument={},
+            truncation_argument={}
+        ):
+        self.tokenizer = tokenizer
+        self.pad_token = pad_token
+        self.pad_id = self.tokenizer.get_vocab()[self.pad_token]
+        self.mask_prob = mask_prob
+        self.mask_token = mask_token
+        self.mask_id = self.tokenizer.get_vocab()[self.mask_token]
+        self.metaspace_token = metaspace_token
+        self.metaspace_id = self.tokenizer.get_vocab()[self.metaspace_token]
+        self.padding_argument = padding_argument
+        self.padding_argument['pad_token'] = self.pad_token
+        self.padding_argument['pad_id'] = self.pad_id
+        self.truncation_argument = truncation_argument
 
-def DataCollatorForUniformMLM():
-    pass
+        if self.padding_argument:
+            self.tokenizer.enable_padding(**padding_argument)
+        if self.truncation_argument:
+            self.tokenizer.enable_truncation(**self.truncation_argument)
 
-def DataCollatorWithPadding():
+    def __call__(self, texts):
+        encodes = self.tokenizer.encode_batch(texts)
+        label_ids = []
+        masked_ids = []
+        attention_masks = []
+        for encode in encodes:
+            label_id = torch.LongTensor(encode.ids)
+            label_ids.append(label_id)
+            masked_id = torch.LongTensor(self._span_mlm(encode.ids))
+            masked_ids.append(masked_id)
+            attention_masks.append(torch.LongTensor(encode.attention_mask))
+
+        batch = {
+            'label_ids' : torch.stack(label_ids),
+            'masked_texts' : torch.stack(masked_ids),
+            'attention_masks' : torch.stack(attention_masks)
+        }
+
+        return batch
+
+    def _span_mlm(self, encode_ids):
+        encode_ids_mlm=[]
+        mask_flag = random.random()<self.mask_prob
+        for encode_id in encode_ids:
+            if encode_id==self.pad_id:
+                encode_ids_mlm.append(self.pad_id)
+                continue
+            elif encode_id==self.metaspace_id:
+                if random.random()<self.mask_prob:
+                    encode_ids_mlm.append(self.mask_id)
+                else:
+                    encode_ids_mlm.append(encode_id)
+                mask_flag = random.random()<self.mask_prob
+                continue
+            else:
+                if mask_flag:
+                    encode_ids_mlm.append(self.mask_id)
+                else:
+                    encode_ids_mlm.append(encode_id)
+                continue
+        return encode_ids_mlm
+        
+
+
+class DataCollatorForMLM:
     pass
